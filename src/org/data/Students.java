aef71a2c82d067e0750d3dc40f6937170640c7e6
diwadiwakar <diwa221998@gmail.com>
2022-05-03 22:07:54 +0530

    java

---
+++ src/org/data/Students.java
@@ -0,0 +1,81 @@
+package org.data;
+
+public class Students {  
+	
+	public static void main(String[] args) {
+		//byte
+		
+		byte Boys=50;
+		byte Girls=25;
+		
+		System.out.println("class boys : "+ Boys);
+		System.out.println("class girls : "+ Girls);
+		System.out.println("1"+Girls);
+		
+	//short
+		
+	short schoolStarts=1975;
+		System.out.println("school starts at :"+schoolStarts);
+		System.out.println("school starts at :"+1975);
+		
+		//integer
+		
+	int schoolLandlineNo =04423654;
+	System.out.println("school mobile no :"+schoolLandlineNo);
+		
+		
+		//long
+	long schoolSqFeet=1230056447892l;
+	System.out.println("school Sq Feet :"+schoolSqFeet);
+		
+		//float
+	float boysAvg=55.2563f;
+	System.out.println("boys percentage :"+boysAvg);
+	
+	float boysratio=10/3f;
+	System.out.println("school boys ratio :"+boysratio);
+	
+	//double
+	double girlsratio=59.23651468d;
+	System.out.println("school girls percentage :"+girlsratio);
+	
+	double girlratio=15/2d;
+	System.out.println("school girls ratio :"+girlratio);
+	
+	//charcter
+	
+	char schoolbranch='1';
+	System.out.println("how many branches :"+schoolbranch);
+	
+	//string
+	
+		String schoolemail="abcschool.1975@Gmail.com";
+		System.out.println("emil Id :"+schoolemail);
+		
+	//boolean
+	
+	boolean privateschool=true;
+	System.out.println("private school :"+privateschool);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		
+		
+		
+		
+		
+	}
+}
+
